c9b32e350368a26d6d4614a09651deefa84e61cd
us-soliman <45239102+us-soliman@users.noreply.github.com>
2021-09-15 21:22:43 +0200

    Update send-email.js

--- js/send-email.js
+++ js/send-email.js
@@ -3,20 +3,23 @@ import { el, all, crtEl } from './func-var.js';
 
 function sendEmail() {
     Email.send({
-        SecureToken : "8cecfff0-c96c-46bd-86bc-89cfbdb57557",
-        
-//         Username: "samoz.designer@gmail.com",
-        To: "samoz.designer@gmail.com",
+        Host: "smtp.gmail.com",
+        Username: "samoz.designer@gmail.com",
+        Password: "nswiczkeetjugvuh",
+        To: 'samoz.designer@gmail.com',
         From: "samoz.designer@gmail.com",
         Subject: el('#client-subject').value,
         Body: `
-        Hello Mr. Ahmed Atef ,
-        my Name is ${el('#client-first').value} ${el('#client-last').value}
-    
+        Name : ${el('#client-first').value} ${el('#client-last').value}
+        <br/>
+        Email : ${el('#client-email').value}
+        <br/>
+        Mobile Number : ${el('#client-number').value}
+        <br/>
+        <br/>
+        The Message => 
+        <br/>
         ${el('#client-message').value}
-        
-        my E-mail : ${el('#client-email').value}
-        my Mobile Number : ${el('#client-number').value}
         `
     })
         .then(function (message) {
